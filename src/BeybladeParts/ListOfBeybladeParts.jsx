2f02c6e45707b0abfa5f950a4c83c1991af2de8b
willjc96 <williamjamescook@aol.com>
2023-02-26 02:50:01 +0000

    add tomy beyblades to list of parts

--- src/BeybladeParts/ListOfBeybladeParts.jsx
+++ src/BeybladeParts/ListOfBeybladeParts.jsx
@@ -3,29 +3,40 @@ import Disk3 from './BeybladePartsImages/Disk3.png';
 import Disk5 from './BeybladePartsImages/Disk5.webp';
 import DiskBoost from './BeybladePartsImages/DiskBoost.png';
 import DiskCentral from './BeybladePartsImages/DiskCentral.png';
+import DiskCiquexQ from './BeybladePartsImages/DiskCiquex-Q.webp';
 import DiskConvert from './BeybladePartsImages/DiskConvert.png';
 import DiskForce from './BeybladePartsImages/DiskForce.png';
 import DiskGravity from './BeybladePartsImages/DiskGravity.png';
 import DiskHeavy from './BeybladePartsImages/DiskHeavy.png';
+import DiskKarma from './BeybladePartsImages/DiskKarma.webp';
+import DiskLegacy from './BeybladePartsImages/DiskLegacy.webp';
 import DiskMagnum from './BeybladePartsImages/DiskMagnum.png';
+import DiskNexusQ from './BeybladePartsImages/DiskNexus-Q.webp';
 import DiskOuter from './BeybladePartsImages/DiskOuter.png';
 import DiskOval from './BeybladePartsImages/DiskOval.png';
+import DiskOver from './BeybladePartsImages/DiskOver.webp';
 import DiskQuarter from './BeybladePartsImages/DiskQuarter.png';
 import DiskRatchet from './BeybladePartsImages/DiskRatchet.png';
 import DiskRatchetP from './BeybladePartsImages/DiskRatchet-P.webp';
 import DiskRing from './BeybladePartsImages/DiskRing.png';
 import DiskSpread from './BeybladePartsImages/DiskSpread.png';
 import DiskSting from './BeybladePartsImages/DiskSting.webp';
+import DiskTapered from './BeybladePartsImages/DiskTapered.webp';
 import DiskUpper from './BeybladePartsImages/DiskUpper.png';
 import DiskYell from './BeybladePartsImages/DiskYell.png';
 
+import DriverAtomicDash from './BeybladePartsImages/DriverAtomicDash.webp';
 import DriverChargeMetalSPM from './BeybladePartsImages/DriverChargeMetal-SPM.png';
 import DriverDefense from './BeybladePartsImages/DriverDefense.png';
+import DriverDestroyDash from './BeybladePartsImages/DriverDestroyDash.webp';
 import DriverDimensioningSP from './BeybladePartsImages/DriverDimensioning-SP.png';
 import DriverEdge from './BeybladePartsImages/DriverEdge.png';
 import DriverFusion from './BeybladePartsImages/DriverFusion.png';
 import DriverFusionalSP from './BeybladePartsImages/DriverFusional-SP.png';
+import DriverHighXtendDash from './BeybladePartsImages/DriverHighXtendDash.webp';
+import DriverKick from './BeybladePartsImages/DriverKick.webp';
 import DriverJaggyQWave from './BeybladePartsImages/DriverJaggy-QWave.png';
+import DriverMetalDestroy from './BeybladePartsImages/DriverMetalDestroy.webp';
 import DriverNeedlingSP from './BeybladePartsImages/DriverNeedling-SP.png';
 import DriverPress from './BeybladePartsImages/DriverPress.png';
 import DriverQuake from './BeybladePartsImages/DriverQuake.png';
@@ -34,7 +45,9 @@ import DriverRise from './BeybladePartsImages/DriverRise.webp';
 import DriverSurvivalSP from './BeybladePartsImages/DriverSurvival-SP.png';
 import DriverSurvive from './BeybladePartsImages/DriverSurvive.png';
 import DriverUltimateReboot from './BeybladePartsImages/DriverUltimateReboot.png';
+import DriverUniteDash from './BeybladePartsImages/DriverUniteDash.webp';
 import DriverVariable from './BeybladePartsImages/DriverVariable.png';
+import DriverVariableDash from './BeybladePartsImages/DriverVariableDash.webp';
 import DriverXceedSPM from './BeybladePartsImages/DriverXceed-SPM.png';
 import DriverXtremeDash from './BeybladePartsImages/DriverXtremeDash.webp';
 import DriverYieldingQVenture from './BeybladePartsImages/DriverYielding-QVenture.png';
@@ -49,6 +62,8 @@ import LayerDranzerFlame from './BeybladePartsImages/LayerDranzerFHasbro.webp';
 import LayerDrigerSlash from './BeybladePartsImages/LayerDrigerSHasbro.webp';
 import LayerFengriffF2 from './BeybladePartsImages/LayerFengriffF2.webp';
 import LayerGaianonG2 from './BeybladePartsImages/LayerGaianonG2.webp';
+import LayerGreatestRaphael from './BeybladePartsImages/LayerGreatestRaphael.png';
+import LayerGuiltyLonginus from './BeybladePartsImages/LayerGuiltyLonginus.png';
 import LayerHorusood from './BeybladePartsImages/LayerHorusoodHasbro.webp';
 import LayerIstrosI2 from './BeybladePartsImages/LayerIstrosI2.webp';
 import LayerJetWyvronW6 from './BeybladePartsImages/LayerJetWyvronW6.png';
@@ -56,12 +71,15 @@ import LayerKolossalFafnirF6 from './BeybladePartsImages/LayerKolossalFafnirF6.p
 import LayerMagmaIfritorI7 from './BeybladePartsImages/LayerMagmaIfritorI7.png';
 import LayerMinoboros from './BeybladePartsImages/LayerMinoborosHasbro.webp';
 import LayerOriginAchillesA6 from './BeybladePartsImages/LayerOriginAchillesA6.png';
+import LayerProminenceValkyrie from './BeybladePartsImages/LayerProminenceValkyrie.png';
 import LayerSpearDullahanD6 from './BeybladePartsImages/LayerSpearDullahanD6.png';
 import LayerSuperHyperionH6 from './BeybladePartsImages/LayerSuperHyperionH6.png';
 import LayerSwordValtryekV5 from './BeybladePartsImages/LayerSwordValtryekV5.png';
 import LayerTwinNemesis from './BeybladePartsImages/LayerTwinNemesis.png';
 import LayerTyrosT6 from './BeybladePartsImages/LayerTyrosT6.png';
+import LayerUltimateValkyrie from './BeybladePartsImages/LayerUltimateValkyrie.png';
 import LayerUnicrest from './BeybladePartsImages/LayerUnicrest.webp';
+import LayerVanishFafnir from './BeybladePartsImages/LayerVanishFafnir.png';
 import LayerWizardFafnir from './BeybladePartsImages/LayerWizardFafnir.png';
 import LayerWorldEvoHeliosH6 from './BeybladePartsImages/LayerWorldEvoHeliosH6.png';
 import LayerYegdrion from './BeybladePartsImages/LayerYegdrion.webp';
@@ -71,325 +89,498 @@ export const listOfParts = [
     "name": "Draciel Shield",
     "type": "Energy Layer",
     "image": LayerDracielSHasbro,
+    "brand": "Hasbro",
   },
   {
     "name": "Dranzer Flame",
     "type": "Energy Layer",
     "image": LayerDranzerFlame,
+    "brand": "Hasbro",
   },
   {
     "name": "Dragoon Fighter",
     "type": "Energy Layer",
     "image": LayerDragoonFighter,
+    "brand": "Hasbro",
   },
   {
     "name": "Driger Slash",
     "type": "Energy Layer",
     "image": LayerDrigerSlash,
+    "brand": "Hasbro",
   },
   {
     "name": "Horusood",
     "type": "Energy Layer",
     "image": LayerHorusood,
+    "brand": "Hasbro",
   },
   {
     "name": "Fengriff F2",
     "type": "Energy Layer",
     "image": LayerFengriffF2,
+    "brand": "Hasbro",
   },
   {
     "name": "Gaianon G2",
     "type": "Energy Layer",
     "image": LayerGaianonG2,
+    "brand": "Hasbro",
   },
   {
     "name": "Istros I2",
     "type": "Energy Layer",
     "image": LayerIstrosI2,
+    "brand": "Hasbro",
   },
   {
     "name": "Minoboros",
     "type": "Energy Layer",
     "image": LayerMinoboros,
+    "brand": "Hasbro",
   },
   {
     "name": "Yegdrion",
     "type": "Energy Layer",
-    "image": LayerYegdrion
+    "image": LayerYegdrion,
+    "brand": "Hasbro",
   },
   {
     "name": "Unicrest",
     "type": "Energy Layer",
     "image": LayerUnicrest,
+    "brand": "Hasbro",
   },
   {
     "name": "Tyros T6",
     "type": "Energy Layer",
     "image": LayerTyrosT6,
+    "brand": "Hasbro",
   },
   {
     "name": "Jet Wyvron W6",
     "type": "Energy Layer",
     "image": LayerJetWyvronW6,
+    "brand": "Hasbro",
   },
   {
     "name": "Origin Achilles A6",
     "type": "Energy Layer",
     "image": LayerOriginAchillesA6,
+    "brand": "Hasbro",
   },
   {
     "name": "World Evo Helios H6",
     "type": "Energy Layer",
     "image": LayerWorldEvoHeliosH6,
+    "brand": "Hasbro",
   },
   {
     "name": "Super Hyperion H6",
     "type": "Energy Layer",
     "image": LayerSuperHyperionH6,
+    "brand": "Hasbro",
   },
   {
     "name": "Kolossal Fafnir F6",
     "type": "Energy Layer",
-    "image": LayerKolossalFafnirF6
+    "image": LayerKolossalFafnirF6,
+    "brand": "Hasbro",
   },
   {
     "name": "Spear Dullahan D6",
     "type": "Energy Layer",
     "image": LayerSpearDullahanD6,
+    "brand": "Hasbro",
   },
   {
     "name": "Brave Roktavor R6",
     "type": "Energy Layer",
     "image": LayerBraveRoktavorR6,
-  },
-  {
-    "name": "Twin Nemesis",
-    "type": "Energy Layer",
-    "image": LayerTwinNemesis,
+    "brand": "Hasbro",
   },
   {
     "name": "Gravity",
     "type": "Forge Disk",
     "image": DiskGravity,
-  },
-  {
-    "name": "3",
-    "type": "Forge Disk",
-    "image": Disk3,
+    "brand": "Hasbro",
   },
   {
     "name": "Ratchet",
     "type": "Forge Disk",
     "image": DiskRatchet,
+    "brand": "Hasbro",
   },
   {
     "name": "Boost",
     "type": "Forge Disk",
     "image": DiskBoost,
+    "brand": "Hasbro",
   },
   {
     "name": "1A-D",
     "type": "Forge Disk",
     "image": Disk1AD,
+    "brand": "Hasbro",
   },
   {
     "name": "Outer",
     "type": "Forge Disk",
     "image": DiskOuter,
+    "brand": "Hasbro",
   },
   {
     "name": "Convert",
     "type": "Forge Disk",
     "image": DiskConvert,
+    "brand": "Hasbro",
   },
   {
     "name": "Magnum",
     "type": "Forge Disk",
     "image": DiskMagnum,
+    "brand": "Hasbro",
   },
   {
     "name": "Ring",
     "type": "Forge Disk",
     "image": DiskRing,
+    "brand": "Hasbro",
   },
   {
     "name": "Oval",
     "type": "Forge Disk",
     "image": DiskOval,
+    "brand": "Hasbro",
   },
   {
     "name": "Upper",
     "type": "Forge Disk",
     "image": DiskUpper,
+    "brand": "Hasbro",
   },
   {
     "name": "Quarter",
     "type": "Forge Disk",
     "image": DiskQuarter,
+    "brand": "Hasbro",
   },
   {
     "name": "Force",
     "type": "Forge Disk",
     "image": DiskForce,
+    "brand": "Hasbro",
   },
   {
     "name": "Spread",
     "type": "Forge Disk",
     "image": DiskSpread,
+    "brand": "Hasbro",
   },
   {
     "name": "Heavy",
     "type": "Forge Disk",
     "image": DiskHeavy,
+    "brand": "Hasbro",
   },
   {
     "name": "Yell",
     "type": "Forge Disk",
     "image": DiskYell,
+    "brand": "Hasbro",
   },
   {
     "name": "Central",
     "type": "Forge Disk",
     "image": DiskCentral,
+    "brand": "Hasbro",
   },
   {
     "name": "Variable",
     "type": "Driver",
     "image": DriverVariable,
+    "brand": "Hasbro",
   },
   {
     "name": "Quake",
     "type": "Driver",
     "image": DriverQuake,
+    "brand": "Hasbro",
   },
   {
     "name": "Edge",
     "type": "Driver",
     "image": DriverEdge,
+    "brand": "Hasbro",
   },
   {
     "name": "Fusion",
     "type": "Driver",
     "image": DriverFusion,
+    "brand": "Hasbro",
   },
   {
     "name": "Survive",
     "type": "Driver",
     "image": DriverSurvive,
+    "brand": "Hasbro",
   },
   {
     "name": "Fusional-SP",
     "type": "Driver",
     "image": DriverFusionalSP,
+    "brand": "Hasbro",
   },
   {
     "name": "Survival-SP",
     "type": "Driver",
     "image": DriverSurvivalSP,
+    "brand": "Hasbro",
   },
   {
     "name": "Dimensioning-SP",
     "type": "Driver",
     "image": DriverDimensioningSP,
+    "brand": "Hasbro",
   },
   {
     "name": "Needling",
     "type": "Driver",
     "image": DriverNeedlingSP,
+    "brand": "Hasbro",
   },
   {
     "name": "Press",
     "type": "Driver",
     "image": DriverPress,
+    "brand": "Hasbro",
   },
   {
     "name": "Defense",
     "type": "Driver",
     "image": DriverDefense,
+    "brand": "Hasbro",
   },
   {
     "name": "Zeta",
     "type": "Driver",
     "image": DriverZeta,
-  },
-  {
-    "name": "Ultimate Reboot",
-    "type": "Driver",
-    "image": DriverUltimateReboot,
+    "brand": "Hasbro",
   },
   {
     "name": "Zoning",
     "type": "Driver",
     "image": DriverZoningSP,
+    "brand": "Hasbro",
   },
   {
     "name": "Charge Metal-SPM",
     "type": "Driver",
     "image": DriverChargeMetalSPM,
+    "brand": "Hasbro",
   },
   {
     "name": "Xceed-SPM",
     "type": "Driver",
     "image": DriverXceedSPM,
+    "brand": "Hasbro",
   },
   {
     "name": "Sting",
     "type": "Forge Disk",
     "image": DiskSting,
+    "brand": "Hasbro",
   },
   {
     "name": "Ratchet-P",
     "type": "Forge Disk",
     "image": DiskRatchetP,
+    "brand": "Hasbro",
   },
   {
     "name": "5",
     "type": "Forge Disk",
     "image": Disk5,
+    "brand": "Hasbro",
   },
   {
     "name": "Wizard Fafnir",
     "type": "Energy Layer",
     "image": LayerWizardFafnir,
+    "brand": "Hasbro",
   },
   {
     "name": "Sword Valtryek V5",
     "type": "Energy Layer",
     "image": LayerSwordValtryekV5,
+    "brand": "Hasbro",
   },
   {
     "name": "Rise-H",
     "type": "Driver",
     "image": DriverRiseH,
+    "brand": "Hasbro",
   },
   {
     "name": "Rise",
     "type": "Driver",
     "image": DriverRise,
+    "brand": "Hasbro",
   },
   {
     "name": "Xtreme'",
     "type": "Driver",
     "image": DriverXtremeDash,
+    "brand": "Hasbro",
   },
   {
     "name": "Yielding-Q Venture",
     "type": "Driver",
     "image": DriverYieldingQVenture,
+    "brand": "Hasbro",
   },
   {
     "name": "Jaggy-Q Wave",
     "type": "Driver",
     "image": DriverJaggyQWave,
+    "brand": "Hasbro",
   },
   {
     "name": "Destruction Belfyre B7",
     "type": "Energy Layer",
     "image": LayerDestructionBelfyreB7,
+    "brand": "Hasbro",
   },
   {
     "name": "Magma Ifritor I7",
     "type": "Energy Layer",
     "image": LayerMagmaIfritorI7,
-  }
+    "brand": "Hasbro",
+  },
+  {
+    "name": "Ciquex-Q",
+    "type": "Forge Disk",
+    "image": DiskCiquexQ,
+    "brand": "Hasbro",
+  },
+  {
+    "name": "Nexus-Q",
+    "type": "Forge Disk",
+    "image": DiskNexusQ,
+    "brand": "Hasbro",
+  },
+  {
+    "name": "Twin Nemesis",
+    "type": "Energy Layer",
+    "image": LayerTwinNemesis,
+    "brand": "Tomy",
+  },
+  {
+    "name": "3",
+    "type": "Forge Disk",
+    "image": Disk3,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Ultimate Reboot",
+    "type": "Driver",
+    "image": DriverUltimateReboot,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Karma",
+    "type": "Disk",
+    "image": DiskKarma,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Legacy",
+    "type": "Disk",
+    "image": DiskLegacy,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Over",
+    "type": "Disk",
+    "image": DiskOver,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Tapered",
+    "type": "Disk",
+    "image": DiskTapered,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Atomic'",
+    "type": "Driver",
+    "image": DriverAtomicDash,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Destroy'",
+    "type": "Driver",
+    "image": DriverDestroyDash,
+    "brand": "Tomy",
+  },
+  {
+    "name": "High Xtend'",
+    "type": "Driver",
+    "image": DriverHighXtendDash,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Kick'",
+    "type": "Driver",
+    "image": DriverKick,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Metal Destroy",
+    "type": "Driver",
+    "image": DriverMetalDestroy,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Unite'",
+    "type": "Driver",
+    "image": DriverUniteDash,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Variable'",
+    "type": "Driver",
+    "image": DriverVariableDash,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Greatest Raphael",
+    "type": "Energy Layer",
+    "image": LayerGreatestRaphael,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Guilty Longinus",
+    "type": "Energy Layer",
+    "image": LayerGuiltyLonginus,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Prominence Valkyrie",
+    "type": "Energy Layer",
+    "image": LayerProminenceValkyrie,
+    "brand": "Tomy",
+  },
+  {
+    "name": "UltimateValkyrie",
+    "type": "Energy Layer",
+    "image": LayerUltimateValkyrie,
+    "brand": "Tomy",
+  },
+  {
+    "name": "Vanish Fafnir",
+    "type": "Energy Layer",
+    "image": LayerVanishFafnir,
+    "brand": "Tomy",
+  },
 ];
